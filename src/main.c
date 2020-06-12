ef532d2d33d13d463e6af87e714ea0cc8d1925ec
Guy Wilson <wilson.guy@gmail.com>
2020-06-12 17:04:32 +0100

    Updated

--- src/main.c
+++ src/main.c
@@ -13,6 +13,7 @@ void handleSignal(int sigNum)
 	switch (sigNum) {
 		case SIGUSR1:
             fprintf(stderr, "Got capture signal...\n");
+            fflush(stderr);
 			return;
 	}
 }
@@ -26,23 +27,28 @@ int main(int argc, char *argv[])
 
 	if (argc > 0) {
         fprintf(stderr, "Got cmdline parameters: ");
+        fflush(stderr);
 
 		for (i = 0;i < argc;i++) {
             fprintf(stderr, "%s ", argv[i]);
+            fflush(stderr);
 
 			if (argv[i][0] == '-') {
 				if (strcmp(&argv[i][1], "version") == 0) {
 					fprintf(stderr, "%s Version: [%s], Build date: [%s]\n\n", pszAppName, getVersion(), getBuildDate());
+                    fflush(stderr);
 					return 0;
 				}
 			}
 		}
 
         fprintf(stderr, "\n\n");
+        fflush(stderr);
 	}
 
 	if (signal(SIGUSR1, &handleSignal) == SIG_ERR) {
 		fprintf(stderr, "Failed to register signal handler for SIGUSR1");
+        fflush(stderr);
 		return -1;
 	}
 
